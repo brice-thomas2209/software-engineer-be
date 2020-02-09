32833d09a3afcd9110ac0071cd1990f94acde894
Brice Thomas <brice@kimberlineducation.com.au>
2020-02-10 01:10:55 +1100

    Adding the user seeder for OAuth

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            UsersTableSeeder::class,
             PropertiesTableSeeder::class,
             AnalyticTypesTableSeeder::class,
             PropertyAnalyticsTableSeeder::class
