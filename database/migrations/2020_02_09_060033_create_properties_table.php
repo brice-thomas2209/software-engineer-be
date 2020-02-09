689f0ae85901b8a401bd6aa431299c9e57180584
Brice Thomas <brice@kimberlineducation.com.au>
2020-02-09 17:07:04 +1100

    add model and migration for properties

---
+++ database/migrations/2020_02_09_060033_create_properties_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->primary();
+            $table->timestamps();
+            $table->uuid('guid')->nullable();
+            $table->string('suburb');
+            $table->string('state');
+            $table->string('country');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}
